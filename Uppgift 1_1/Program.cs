46e633e04496b6377e15863e0d76e85c6e1e12e0
elgo07001 <elgo07001@E-PW05C37M>
2024-09-11 12:26:48 +0200

    Add project files.

---
+++ Uppgift 1_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace uppgift1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Den kommer skriva "Du är nästan klar med kapitel 1."
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+            //Den kommer skriva "Du har lärt dig skriva ut tecken som \" och \\."
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\.");
+
+        }
+    }
+}
